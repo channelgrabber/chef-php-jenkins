57c4de8af22dfefd7f6258d83d64a61ac9214cf3
Richard Heelin <richardh@channelgrabber.com>
2014-03-03 12:16:53 +0000

    CGIV-1650: Setting composer environment variables

--- recipes/composer.rb
+++ recipes/composer.rb
@@ -1,25 +1,24 @@
 include_recipe "composer::install"
 
-packages = {
-    "phpunit/phpunit=3.7.*" => "phpunit",
-    "squizlabs/php_codesniffer=1.5.*" => "phpcs",
-    "phploc/phploc=2.0.*" => "phploc",
-    "pdepend/pdepend=1.1.*" => "pdepend",
-    "phpmd/phpmd=1.4.*" => "phpmd",
-    "sebastian/phpcpd=2.0.*" => "phpcpd",
-    "theseer/phpdox=0.6.*" => "phpdox"
-}
+packages = [
+    "phpunit/phpunit=3.7.*",
+    "squizlabs/php_codesniffer=1.5.*",
+    "phploc/phploc=2.0.*",
+    "pdepend/pdepend=1.1.*",
+    "phpmd/phpmd=1.4.*",
+    "sebastian/phpcpd=2.0.*",
+    "theseer/phpdox=0.6.*"
+]
 
-packages.each do |require, executable|
+packages.each do |require|
     execute "composer global require '#{require}'" do
         user node['jenkins']['server']['user']
         group node['jenkins']['server']['group']
+        environment ({
+            'HOME' => #node['jenkins']['server']['home'],
+            'COMPOSER_HOME' => File.join(#node['jenkins']['server']['home'], '.composer'),
+            'COMPOSER_BIN_DIR' => '/usr/local/bin'
+        })
         action :run
-    end
-
-    link File.join("/usr/local/bin", executable) do
-        to File.join(File.expand_path("$COMPOSER_HOME/vendor/bin"), executable)
-        owner node['jenkins']['server']['user']
-        group node['jenkins']['server']['group']
     end
 end
